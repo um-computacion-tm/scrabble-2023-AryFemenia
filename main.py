a0296f5d2378ba0c7944c29dc8799f8861f9105c
AryFemenia <ad.femenia@alumno.um.edu.ar>
2023-10-04 22:08:35 -0300

    added player count

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ def main():
     print("Bienvenido")
     #players_count = input("Ingrese el numero de jugadores: ")
 
+    player_count = get_player_count()
+    game = ScrabbleGame(player_count)
+    while game.is_playing():
+        show_board(game.get_board())
+        show_player(*game.get_current_player())
+        word, coords, orientation = get_inputs()
+        try:
+            game.play(word, coords, orientation)
+        except Exception as e:
+            print(e)
+
     scrabble_board = Board()
 
     print("Tablero de Scrabble:")
